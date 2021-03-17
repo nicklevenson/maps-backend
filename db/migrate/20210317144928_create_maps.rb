32d11da1059b883392524401c5f58caded30e8d3
Nicholas Levenson <nicklevenson@gmail.com>
2021-03-17 08:05:21 -0700

    boolean:

--- db/migrate/20210317144928_create_maps.rb
+++ db/migrate/20210317144928_create_maps.rb
@@ -3,7 +3,7 @@ class CreateMaps < ActiveRecord::Migration[6.1]
     create_table :maps do |t|
       t.string :title
       t.string :description
-      t.boolean :public
+      t.boolean :public, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_17_145011) do
   create_table "maps", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.boolean "public"
+    t.boolean "public", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
